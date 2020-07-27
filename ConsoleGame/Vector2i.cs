d6009091b2c34073867d2ded6ebb5f1e3ded3d94
Paweł Krolik <pawel8krolik@gmail.com>
2020-07-27 12:10:41 +0200

    Add Vector2i class

---
+++ ConsoleGame/Vector2i.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleGame
+{
+    public class Vector2i
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2i() { }
+
+        public Vector2i(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public bool CompareTo(Vector2i Other)
+        {
+            return (this.X == Other.X && this.Y == Other.Y);
+        }
+
+    }
+
+
+}
---
+++ ConsoleGame_Tests/Vector2i_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame_Tests
+{
+    [TestClass]
+    public class Vector2i_Tests
+    {
+
+        [TestMethod]
+        public void CompareTo_Test()
+        {
+            var First = new ConsoleGame.Vector2i(4, 7);
+            var Second = new ConsoleGame.Vector2i(4, 7);
+            var Third = new ConsoleGame.Vector2i(2, 7);
+
+            Assert.IsTrue(First.CompareTo(Second));
+            Assert.IsFalse(Second.CompareTo(Third));
+        }
+
+    }
+}
