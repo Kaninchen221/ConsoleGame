84115e54bbf31a1c8637cf786ecddde5204dbad3
Paweł Krolik <pawel8krolik@gmail.com>
2020-07-27 18:16:02 +0200

    Add Utility class with extension method 'ReplaceAt' for string class

---
+++ ConsoleGame/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame
+{
+    public static class Utility
+    {
+        public static string ReplaceAt(this string input, char newChar, int index)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input can't be null");
+            }
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
+
+    }
+}
---
+++ ConsoleGame_Tests/Utility_Tests.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame_Tests
+{
+    [TestClass]
+    public class Utility_Tests
+    {
+        [TestMethod]
+        public void ReplaceAt_Test()
+        {
+            string String = new string("01234 6789");
+            String = String.ReplaceAt('5', 5);
+
+            Assert.AreEqual('5', String[5]);
+        }
+
+    }
+}
