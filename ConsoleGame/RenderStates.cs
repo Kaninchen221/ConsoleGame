b7a11285af31def440e66bb46c7fc66ab93c6b70
Paweł Krolik <pawel8krolik@gmail.com>
2020-07-29 18:38:52 +0200

    Add render states

---
+++ ConsoleGame/RenderStates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame
+{
+    class RenderStates
+    {
+        public Vector2i Position { get; set; } = new Vector2i();
+        public Texture Texture { get; set; } = new Texture();
+        public ConsoleColor Color { get; set; } = ConsoleColor.White;
+    }
+}
