28bb007ad8e978d7c75c7565140ee33ef25e251d
Paweł Krolik <pawel8krolik@gmail.com>
2020-07-29 22:34:56 +0200

    Mark class as public

--- ConsoleGame/RenderStates.cs
+++ ConsoleGame/RenderStates.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace ConsoleGame
 {
-    class RenderStates
+    public class RenderStates
     {
-        public Vector2i Position { get; set; } = new Vector2i();
+        public Vector2i Position { get; set; } = new Vector2i(0, 0);
         public Texture Texture { get; set; } = new Texture();
         public ConsoleColor Color { get; set; } = ConsoleColor.White;
     }
