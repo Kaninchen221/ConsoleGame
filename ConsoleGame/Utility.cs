76f60e806275fef70e70668a957d58ef33fe8467
Paweł Krolik <pawel8krolik@gmail.com>
2020-08-01 21:23:05 +0200

    All arguments of functions starts with upper case and add extension method for string 'ReplaceAt' for string as argument

--- ConsoleGame/Utility.cs
+++ ConsoleGame/Utility.cs
@@ -6,15 +6,40 @@ namespace ConsoleGame
 {
     public static class Utility
     {
-        public static string ReplaceAt(this string input, char newChar, int index)
+        public static string ReplaceAt(this string Input, char NewChar, int Index)
         {
-            if (input == null)
+            if (Input == null)
             {
                 throw new ArgumentNullException("input can't be null");
             }
-            char[] chars = input.ToCharArray();
-            chars[index] = newChar;
-            return new string(chars);
+
+            char[] Chars = Input.ToCharArray();
+            Chars[Index] = NewChar;
+            return new string(Chars);
+        }
+
+        public static string ReplaceAt(this string Input, string String, int Index)
+        {
+            if (Input == null || String == null)
+                throw new ArgumentNullException("input can't be null");
+
+            if (String.Length + Index >= Input.Length)
+                throw new ArgumentException("Can't fit String at this index");
+
+
+            char[] Chars = Input.ToCharArray();
+
+            int InputChar = Index;
+            int StringChar = 0;
+            while(StringChar < String.Length)
+            {
+                Chars[InputChar] = String[StringChar];
+
+                InputChar += 1;
+                StringChar += 1;
+            }
+
+            return new string(Chars);
         }
 
     }
