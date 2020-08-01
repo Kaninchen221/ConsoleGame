a6e00bda6db21e8a42b353a87d47676b5a3889d7
Paweł Krolik <pawel8krolik@gmail.com>
2020-08-01 21:26:08 +0200

    Add test for 'ReplaceAt' extension method when the first arg is string

--- ConsoleGame_Tests/Utility_Tests.cs
+++ ConsoleGame_Tests/Utility_Tests.cs
@@ -10,7 +10,7 @@ namespace ConsoleGame_Tests
     public class Utility_Tests
     {
         [TestMethod]
-        public void ReplaceAt_Test()
+        public void ReplaceAt_CharTest()
         {
             string String = new string("01234 6789");
             String = String.ReplaceAt('5', 5);
@@ -18,5 +18,15 @@ namespace ConsoleGame_Tests
             Assert.AreEqual('5', String[5]);
         }
 
+        [TestMethod]
+        public void ReplaceAt_StringTest()
+        {
+            string String = new string("0123   789");
+            string Input = new string("456");
+
+            var NewString = String.ReplaceAt(Input, 4);
+
+            Assert.AreEqual(Input, NewString.Substring(4, 3));
+        }
     }
 }
