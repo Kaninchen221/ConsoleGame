3bfcd89b88bd2f2bb0718a706448ef59972dc49d
Paweł Krolik <pawel8krolik@gmail.com>
2020-08-03 22:29:52 +0200

    Interface for Renderers

---
+++ ConsoleGame/IRenderTarget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame
+{
+    public interface IRenderTarget
+    {
+        public void Draw(char Char, RenderStates States);
+        public void Draw(string Line, RenderStates States);
+        public void Draw(Texture Texture, RenderStates States);
+    }
+}
