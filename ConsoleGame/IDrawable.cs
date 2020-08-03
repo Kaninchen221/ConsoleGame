65810f6e21ef9c956c7b70b8639df858c32d64bd
Paweł Krolik <pawel8krolik@gmail.com>
2020-08-03 22:26:00 +0200

    Interface for drawable classes

---
+++ ConsoleGame/IDrawable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame
+{
+    public interface IDrawable
+    {
+        public void Draw(IRenderTarget Target, RenderStates States);
+    }
+}
