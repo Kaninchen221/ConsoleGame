b1905a6b737017e9444996357feb0899ccb47a23
Paweł Krolik <pawel8krolik@gmail.com>
2020-08-03 16:05:48 +0200

    Remove texture from RenderStates

--- ConsoleGame/RenderStates.cs
+++ ConsoleGame/RenderStates.cs
@@ -7,7 +7,6 @@ namespace ConsoleGame
     public class RenderStates
     {
         public Vector2i Position { get; set; } = new Vector2i(0, 0);
-        public Texture Texture { get; set; } = new Texture();
         public ConsoleColor Color { get; set; } = ConsoleColor.White;
     }
 }
